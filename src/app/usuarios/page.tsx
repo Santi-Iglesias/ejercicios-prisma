9d1e0e6277828d1610ad09bbad275261fe29375e
Santiago Iglesias <santiigle07@gmail.com>
2023-10-07 01:01:14 -0300

    muestro las publicaciones :)

--- src/app/usuarios/page.tsx
+++ src/app/usuarios/page.tsx
@@ -1,9 +1,19 @@
+import { Publicacion } from "@prisma/client";
+
 async function obtenerUsuarios() {
     const respuesta = await fetch("http://localhost:3000/api/usuarios", {
         cache: "no-store",
     });
     const datos = await respuesta.json();
     return datos;
+}
+
+async function obtenerPublicaciones() {
+    const resp = await fetch("http://localhost:3000/api/usuarios/publicaciones", {
+        cache: "no-store",
+    });
+    const dato = await resp.json();
+    return dato;
 }
 
 type Usuario = {
@@ -13,10 +23,12 @@ type Usuario = {
     email: string;
     password: string;
     activo: boolean;
+    Publicacion: Publicacion[]
 };
 
 export default async function Pagina() {
     const usuarios = await obtenerUsuarios();
+    const publicaciones = await obtenerPublicaciones();
 
     return (
         <section>
@@ -26,9 +38,18 @@ export default async function Pagina() {
                 {usuarios
                     .filter((usuario: Usuario) => usuario.edad > 25)
                     .map((usuario: Usuario) => (
+                        <main>
                         <li key={usuario.id}>
                             {usuario.nombre} - {usuario.edad}
                         </li>
+                        <li>
+                        {publicaciones.map((publicacion: Publicacion) => (
+                            <li>
+                                {publicacion.titulo} - {publicacion.contenido}
+                            </li>
+                        ))}
+                        </li>
+                        </main>
                     ))}
             </ul>
         </section>
